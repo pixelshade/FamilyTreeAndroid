5175bfc441a6540de734ef8e7d0340f6cfb91599
pixelshade <psuldo@gmail.com>
2015-08-03 09:08:31 +0200

    started upload manager

--- app/src/main/java/studios/pixelshade/familytree/UploadManager.java
+++ app/src/main/java/studios/pixelshade/familytree/UploadManager.java
@@ -1,35 +1,155 @@
 package studios.pixelshade.familytree;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.CoreProtocolPNames;
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.DataOutputStream;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 
 /**
  * Created by pixelshade on 31/07/15.
  */
 public class UploadManager {
 
+    int serverResponseCode = 0;
 
-    public void upload(String filepath) throws IOException
-    {
-        HttpClient httpclient = new DefaultHttpClient();
-        httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-        HttpPost httppost = new HttpPost("url");
-        File file = new File(filepath);
-        MultipartEntity mpEntity = new MultipartEntity();
-        ContentBody cbFile = new FileBody(file, "image/jpeg");
-        mpEntity.addPart("userfile", cbFile);
-        httppost.setEntity(mpEntity);
-        System.out.println("executing request " + httppost.getRequestLine());
-        HttpResponse response = httpclient.execute(httppost);
-        HttpEntity resEntity = response.getEntity();
-        // check the response and do what is required
-    }
+
+    String upLoadServerUri = null;
+
+    /**********  File Path *************/
+    final String uploadFilePath = "/mnt/sdcard/";
+    final String uploadFileName = "service_lifecycle.png";
+
+
+
+//    public int uploadFile(String sourceFileUri, ProgressDialog dialog) {
+//
+//
+//        String fileName = sourceFileUri;
+//
+//        HttpURLConnection conn = null;
+//        DataOutputStream dos = null;
+//        String lineEnd = "\r\n";
+//        String twoHyphens = "--";
+//        String boundary = "*****";
+//        int bytesRead, bytesAvailable, bufferSize;
+//        byte[] buffer;
+//        int maxBufferSize = 1 * 1024 * 1024;
+//        File sourceFile = new File(sourceFileUri);
+//
+//        if (!sourceFile.isFile()) {
+//
+//            dialog.dismiss();
+//
+//            Log.e("uploadFile", "Source File not exist :"
+//                    + uploadFilePath + "" + uploadFileName);
+//
+//            return 0;
+//
+//        }
+//        else
+//        {
+//            try {
+//
+//                // open a URL connection to the Servlet
+//                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+//                URL url = new URL(upLoadServerUri);
+//
+//                // Open a HTTP  connection to  the URL
+//                conn = (HttpURLConnection) url.openConnection();
+//                conn.setDoInput(true); // Allow Inputs
+//                conn.setDoOutput(true); // Allow Outputs
+//                conn.setUseCaches(false); // Don't use a Cached Copy
+//                conn.setRequestMethod("POST");
+//                conn.setRequestProperty("Connection", "Keep-Alive");
+//                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+//                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+//                conn.setRequestProperty("uploaded_file", fileName);
+//
+//                dos = new DataOutputStream(conn.getOutputStream());
+//
+//                dos.writeBytes(twoHyphens + boundary + lineEnd);
+//                dos.writeBytes("Content-Disposition: form-data; name="+uploaded_file+";filename=""
+//                                + fileName + """ + lineEnd);
+//
+//                        dos.writeBytes(lineEnd);
+//
+//                // create a buffer of  maximum size
+//                bytesAvailable = fileInputStream.available();
+//
+//                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+//                buffer = new byte[bufferSize];
+//
+//                // read file and write it into form...
+//                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+//
+//                while (bytesRead > 0) {
+//
+//                    dos.write(buffer, 0, bufferSize);
+//                    bytesAvailable = fileInputStream.available();
+//                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+//                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+//
+//                }
+//
+//                // send multipart form data necesssary after file data...
+//                dos.writeBytes(lineEnd);
+//                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+//
+//                // Responses from the server (code and message)
+//                serverResponseCode = conn.getResponseCode();
+//                String serverResponseMessage = conn.getResponseMessage();
+//
+//                Log.i("uploadFile", "HTTP Response is : "
+//                        + serverResponseMessage + ": " + serverResponseCode);
+//
+//                if(serverResponseCode == 200){
+//
+//
+//
+//                            Toast.makeText(UploadToServer.this, "File Upload Complete.",
+//                                    Toast.LENGTH_SHORT).show();
+//
+//                }
+//
+//                //close the streams //
+//                fileInputStream.close();
+//                dos.flush();
+//                dos.close();
+//
+//            } catch (MalformedURLException ex) {
+//
+//                dialog.dismiss();
+//                ex.printStackTrace();
+//
+//
+//                        Toast.makeText(UploadToServer.this, "MalformedURLException",
+//                                Toast.LENGTH_SHORT).show();
+//
+//
+//                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+//            } catch (Exception e) {
+//
+//                dialog.dismiss();
+//                e.printStackTrace();
+//
+//
+//                        Toast.makeText(UploadToServer.this, "Got Exception : see logcat ",
+//                                Toast.LENGTH_SHORT).show();
+//
+//                Log.e("Upload file to server Exception", "Exception : "
+//                        + e.getMessage(), e);
+//            }
+//            dialog.dismiss();
+//            return serverResponseCode;
+//
+//        } // End else block
+//    }
 }
